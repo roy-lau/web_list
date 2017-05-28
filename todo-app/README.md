9c22881dcc1fc19d9db14086c3b01b3194ee9b5c
roy-lau <roy-lau@foxmail.com>
2017-05-28 23:22:05 +0800

    jq-todo-app业务逻辑之删除-----2017.5.28 23:35

--- todo-app/README.md
+++ todo-app/README.md
@@ -1,3 +1,9 @@
 ##  jq-todo-app
 
-有网时，百度一下 详细了解了解box-sizing: border-box;
+有网时，百度一下 详细了解了解 
+
+1. box-sizing: border-box;
+
+<input type="text" autofocus  autocomplete="off" />  // 关闭之前输入过的内容
+
+[老师项目地址](http://todolist.t.imooc.io/)
--- todo-app/js/script.js
+++ todo-app/js/script.js
@@ -1,4 +1,103 @@
-;(function(){
+;
+(function() {
     "use scrict"
-    
+    var $formAddTask = $(".add-task"),
+        $deleteTask,
+        taskList = {};
+
+    init()
+
+    $formAddTask.on('submit', onAddTaskFormSubmit)
+
+    function onAddTaskFormSubmit(e){
+        var newTask = {},
+            $input;
+        // 禁用默认行为
+        e.preventDefault();
+        // 获取新task的值
+        $input = $(this).find('input[name="content"]');
+        newTask.content = $input.val();
+        // 如果task的值为空，直接返回。否则继续执行
+        if (!newTask.content) return;
+
+        // 存入新task
+        if (addTask(newTask)) {
+            renderTaskList();
+            $input.val(null) // 添加完成后清空
+        };
+
+    }
+
+    // 查找并监听所有删除按钮的点击事件
+    function listenTaskDelete(){
+        $deleteTask.on("click", function() {
+            var $this = $(this),
+            // 找到并删除按钮的task元素
+                $Item = $this.parent(),
+                index = $Item.data("index"),
+                // 确认删除 
+                tmp = confirm("确定删除？");
+            tmp ? deleteTask(index) : null;
+            
+        })
+    }
+
+    function addTask(newTask) {
+        // 将task push进 taskList
+        taskList.push(newTask)
+        updataTask()
+        return true;
+    }
+
+    function updataTask() {
+        // 更新localStorage,并更新渲染tpl
+        store.set("taskList", taskList);
+        renderTaskList();
+    }
+
+    // 删除一条task
+    function deleteTask(index) {
+        // 如果没有index，或者taskList的index不存在，则直接return
+        if (index ===undefined || !taskList[index]) return;
+        console.log(taskList[index])
+
+        delete taskList[index];
+
+        updataTask();
+    }
+
+    function init() {
+        taskList = store.get("taskList") || [];
+        if (taskList.length) {
+            renderTaskList()
+        }
+    }
+
+    // 渲染全部的task模板
+    function renderTaskList() {
+        var $taskList = $('.task-list');
+        $taskList.html("")
+        for (var i = 0; i < taskList.length; i++) {
+            var $task = renderTaskItem(taskList[i], i);
+            $taskList.append($task)
+        };
+        $deleteTask = $(".auchor.delete");
+        listenTaskDelete()
+    }
+
+    // 渲染单条task模板
+    function renderTaskItem(data, index) {
+        if (!data　|| !index) return;
+        var listItemTpl =
+            '<div class="task-list">' +
+            '<div class="task-item"  data-index="' + index + '">' +
+            '<span><input type="checkbox"></span>' +
+            '<span class="task-content">' + data.content + '</span>' +
+            '<span class="auchor"> 详细  </span>' +
+            '<span class="auchor delete"> 删除 &nbsp;</span>' +
+            '</div>' +
+            '</div>';
+
+        return $(listItemTpl);
+    }
 })()
