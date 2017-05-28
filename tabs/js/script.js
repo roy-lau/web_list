aa16e8602273e0b8811556ef8f8055c8ad68191e
roy-lau <roy-lau@foxmail.com>
2017-05-28 14:52:22 +0800

    tabs选项卡切换---自动切换

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -1,4 +1,5 @@
-;(function($) {
+;
+(function($) {
 
     var Tab = function(tab) {
         var self = this;
@@ -19,69 +20,90 @@
 
         // 保存tabs标签列表&&报存对应的内容列表
         this.tabItems = this.tab.find("ul.tab-nav li");
-        this.contentItems = this.tab.find("div.content-wrap div.content-item");  // 坑-- 标签名，属性一定要正确
+        this.contentItems = this.tab.find("div.content-wrap div.content-item"); // 坑-- 标签名，属性一定要正确
 
         // 保存config配置参数
         var config = this.config;
-        if(config.triggerType === "click"){
 
-        	this.tabItems.bind(config.triggerType,function(){
-        		self.invoke($(this));
-        	})
+        //---------- 判断切换事件
+        if (config.triggerType === "click") {
+            this.tabItems.bind(config.triggerType, function() {
+                self.invoke($(this));
+            })
+        } else if (config.triggerType === "mouseover") {
+            this.tabItems.bind(config.triggerType, function() {
+                self.invoke($(this));
+            })
+        } else if (config.triggerType === "mouseover" || config.triggerType !== "click") { // 规避传如入事件参数错误
+            this.tabItems.bind("mouseover", function() {
+                self.invoke($(this));
+            })
+        }
 
-        }else if(config.triggerType === "mouseover"){
-        	
-        	this.tabItems.bind(config.triggerType,function(){
-        		self.invoke($(this));
-        		
-        	})
 
-        }else if(config.triggerType === "mouseover" || config.triggerType !== "click"){  // 规避传如入事件参数错误
-
-        	this.tabItems.bind("mouseover",function(){
-        		self.invoke($(this));
-        		
-        	})
-
+        //---------- 自动切换功能
+        if (config.auto) {
+        	// 定义一个全局的定时器
+        	this.timer = null;
+        	// 计数器
+        	this.loop = 0;
+        	// 自动间隔时间切换的方法
+        	this.autoPlay()
         }
     }
 
     Tab.prototype = {
+    	// 自动间隔时间切换的方法
+    	autoPlay: function(){
+    		var self = this,
+    		tabItems = this.tabItems,	 // 临时保存tab列表
+    		tabLength = tabItems.size(), // tab的总个数（长度）
+    		config = this.config;		 // 配置参数
 
-    	// 事件驱动函数
-    	invoke: function(currentTab){	// currentTab当前的tab
-    		var self = this;
-    		/*
+    		this.timer = window.setInterval(function(){
+    			self.loop++;
+    			if (self.loop >= tabLength ) {
+    				self.loop = 0;
+    			}
+    			tabItems.eq(self.loop).trigger(config.triggerType);  // 触发配置参数上的事件;
+
+    		},config.auto)
+    	},
+        // 事件驱动函数
+        invoke: function(currentTab) { // currentTab当前的tab
+            var self = this;
+            /*
 				1. 执行tab选中状态选中状态加上actived
 				2. 切换对应的内容，根据配置参数effect是default还是fade
     		*/
 
-    		// 获取图片的下标
-    		var index = currentTab.index()
-    		// 给选中的li添加actived样式，同时 移除兄弟节点的actived样式
-    		currentTab.addClass("actived").siblings().removeClass("actived");
+            // 获取图片的下标
+            var index = currentTab.index()
+                // 给选中的li添加actived样式，同时 移除兄弟节点的actived样式
+            currentTab.addClass("actived").siblings().removeClass("actived");
 
-    		var effect = this.config.effect;
-    		var contentItems = this.contentItems;
+            var effect = this.config.effect;
+            var contentItems = this.contentItems;
 
-    		// 判断切换方式
-    		if (effect === "default") {
-    			contentItems.eq(index).addClass("current").siblings().removeClass("current")
-    		}else if(effect === "fade"){
-    			contentItems.eq(index).fadeIn().siblings().fadeOut()
+            //------  判断切换方式
+            if (effect === "default") {
+                contentItems.eq(index).addClass("current").siblings().removeClass("current")
+            } else if (effect === "fade") {
+                contentItems.eq(index).fadeIn().siblings().fadeOut()
 
-    		}else if(effect === "default"||effect !== "fade"){
-    			contentItems.eq(index).addClass("current").siblings().removeClass("current")
-    		}
+            } else if (effect === "default" || effect !== "fade") {
+                contentItems.eq(index).addClass("current").siblings().removeClass("current")
+            }
 
-    	},
+
+        },
 
         // 获取HTML内的配置参数
         getconfig: function() {
             // 获取HTML内的data-config配置
             var config = this.tab.data("config");
 
-            // 判断HTMl内是否有配置参数
+            //-------- 判断HTMl内是否有配置参数
             if (config && config !== "") {
                 // 如果HTML内有配置参数,将获取到的配置参数转化为json对象--$.parseJSON(config)高版本jq不需要转化
                 return config
