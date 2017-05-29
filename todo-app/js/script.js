068315a056d19edc1117f7170e5f750d5a513bda
roy-lau <roy-lau@foxmail.com>
2017-05-29 19:00:10 +0800

    :clock1::clock3::zap:定时任务,每天自动上传项目:zap::clock5::clock6:---2017/05/29-19:00:08

--- todo-app/js/script.js
+++ todo-app/js/script.js
@@ -9,7 +9,8 @@
         $updataForm,
         $taskDetailContent,
         $taskDetailContentInput,
-        currentIdex,
+        $checkboxComplete,
+        currentIndex,
     taskList = {};
 
     init()
@@ -18,7 +19,8 @@
     $taskDetailMask.on('click', fadeOutTaskDetail)
 
     function onAddTaskFormSubmit(e) {
-        var newTask = {},$input;
+        var newTask = {},
+            $input;
         // 禁用默认行为
         e.preventDefault();
         // 获取新task的值
@@ -37,8 +39,8 @@
     // 监听打开task事件
     function listenTaskDetail() {
         var index;
-        $('.task-item').on('click',function(){
-            index = $(this).data("index")
+        $('.task-item').on('click', function() {
+            index = $(this).data("index");
             fadeInTaskDetail(index)
         })
         $detailTask.on("click", function() {
@@ -52,7 +54,7 @@
     function fadeInTaskDetail(i) {
         // 生成详情模板
         renderTaskDetail(i);
-        currentIdex = i;
+        currentIndex = i;
         // 显示详情模板（默认隐藏）
         $taskDetail.fadeIn();
         $taskDetailMask.fadeIn();
@@ -63,59 +65,58 @@
         $taskDetailMask.fadeOut();
     }
     // 更新task详细信息
-    function updataTask(i, data){
+    function updataTask(i, data) {
         if (!i || !taskList[i]) return;
-        taskList[i] = data;
+        taskList[i] = $.extend({}, taskList[i], data);
         renderTaskData()
     }
     // 渲染指定task的详细信息
     function renderTaskDetail(i) {
         if (!i === undefined || !taskList[i]) return;
         var item = taskList[i];
-        console.log(item)
         var tpl =
             '<form>' +
             '<div class="content">' +
             item.content +
             '</div>' +
-            '<div class="input-item">'+
-            '<input type="text" name="content" value="'+ (item.content || '') +'" style="display:none;" /></div>' +
+            '<div class="input-item">' +
+            '<input type="text" name="content" value="' + (item.content || '') + '" style="display:none;" /></div>' +
             '<div class="desc input-item">' +
-            '<textarea name="desc">'+ (item.desc || '')+'</textarea>' +
+            '<textarea name="desc">' + (item.desc || '') + '</textarea>' +
             '</div>' +
             '<div class="remind input-item">' +
-            '<input name="remind-date" type="date" value="'+ (item.remindDate || '') +'">' +
+            '<input name="remind-date" type="date" value="' + (item.remindDate || '') + '">' +
             '</div>' +
             '<button type="submit">更新</button>' +
             '</form>';
-            // 清空task模板
-            $taskDetail.html(null);
-            // 用新模板替换旧模板
-            $taskDetail.html(tpl);
-            // 选中form元素，因为之后会监听submit事件
-            $updataForm =  $taskDetail.find("form");
-            // 选中显示task内容元素
-            $taskDetailContent = $updataForm.find(".content");
-            // 选中task input内容元素
-            $taskDetailContentInput = $updataForm.find("[name=content]");
-            // 双击内容元素显示input，隐藏自己
-            $taskDetailContent.on("dblclick", function(){
-               $taskDetailContentInput.show()
-               $taskDetailContent.hide()
-            })
+        // 清空task模板
+        $taskDetail.html(null);
+        // 用新模板替换旧模板
+        $taskDetail.html(tpl);
+        // 选中form元素，因为之后会监听submit事件
+        $updataForm = $taskDetail.find("form");
+        // 选中显示task内容元素
+        $taskDetailContent = $updataForm.find(".content");
+        // 选中task input内容元素
+        $taskDetailContentInput = $updataForm.find("[name=content]");
+        // 双击内容元素显示input，隐藏自己
+        $taskDetailContent.on("dblclick", function() {
+            $taskDetailContentInput.show()
+            $taskDetailContent.hide()
+        })
 
-            $updataForm.on('submit',function(e){
-                e.preventDefault();
-                var data = {};
-                // 获取表单中各个input的值
-                data.content = $(this).find('[name=content]').val();
-                data.desc = $(this).find('[name=desc]').val();
-                data.remindDate = $(this).find('[name=remind-date]').val();
-                updataTask(i,data);
-                fadeOutTaskDetail();
-            })
+        $updataForm.on('submit', function(e) {
+            e.preventDefault();
+            var data = {};
+            // 获取表单中各个input的值
+            data.content = $(this).find('[name=content]').val();
+            data.desc = $(this).find('[name=desc]').val();
+            data.remindDate = $(this).find('[name=remind-date]').val();
+            updataTask(i, data);
+            fadeOutTaskDetail();
+        })
 
-            
+
     }
 
     // 查找并监听所有删除按钮的点击事件
@@ -130,6 +131,26 @@
             tmp ? deleteTask(index) : null;
 
         })
+    }
+
+    // 监听完成task事件
+    function listenCheckboxComplete() {
+        $checkboxComplete.on('click', function() {
+            var $this = $(this),
+                isComplete = $this.is(':checked'),
+                index = $this.parent().parent().data("index");
+
+            var item = getStore(index);
+            if (item.complete) {
+                updataTask(index, { complete: false });
+            }else{
+                updataTask(index, { complete: true });
+            }
+        })
+    }
+
+    function getStore(index){
+        return store.get("taskList")[index]
     }
 
     function addTask(newTask) {
@@ -172,17 +193,19 @@
         };
         $deleteTask = $(".auchor.delete");
         $detailTask = $(".auchor.detail");
+        $checkboxComplete = $(".task-list .complete");
         listenTaskDelete();
         listenTaskDetail();
+        listenCheckboxComplete();
     }
 
     // 渲染单条task模板
     function renderTaskItem(data, index) {
-        if (!data　 || !index) return;
+        if (!data || !index) return;
         var listItemTpl =
             '<div class="task-list">' +
             '<div class="task-item"  data-index="' + index + '">' +
-            '<span><input type="checkbox"></span>' +
+            '<span><input class="complete" '+ (data.complete ? "checked" : "")+' type="checkbox"></span>' +
             '<span class="task-content">' + data.content + '</span>' +
             '<span class="auchor detail"> 详细  </span>' +
             '<span class="auchor delete"> 删除 &nbsp;</span>' +
