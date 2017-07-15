a3acc0c62b5618b1f010024a224a6b4ff07f73c8
roy-lau <roy-lau@foxmail.com>
2017-07-16 00:45:42 +0800

    angular-laguo--小问题，遇事不冷静，浪费这么长时间，谨记！

--- angularjs-lagou/script/controller/companyCtr.js
+++ angularjs-lagou/script/controller/companyCtr.js
@@ -1,7 +1,10 @@
 'use strict';
 
-angular.module('app').controller('companyCtr', ['$scope', '$state', '$http', function($scope, $state, $http) {
+angular.module('app').controller('companyCtr',
+	['$scope', '$state', '$http', function($scope, $state, $http) {
+
     $http.get('public/data/company.json?id' + $state.params.id).success(function(res) {
         $scope.company = res;
+        console.log(res)
     })
 }])
