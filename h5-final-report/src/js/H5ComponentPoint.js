7a84addaf02c4278518d3b533b322dc4ae0ff0eb
roy-lau <roy-lau@foxmail.com>
2017-10-03 18:46:45 +0800

    h5-final-report：散点图组件开发

---
+++ h5-final-report/src/js/H5ComponentPoint.js
@@ -0,0 +1,30 @@
+/* 散点图组件对象 */
+var H5ComponentPoint = function(name, cfg) {
+    var component = new H5ComponentBase(name, cfg);
+
+    var　 base = cfg.data[0][1]; // 以第一个数据的 比例为大小的 100%
+
+    // 输出每个Point
+    $.each(cfg.data, function(i, item) {
+        var point = $('<div class="point point_' + i + '">'),
+            name = $('<div class="name">' + item[0] + '</div>'),
+            rate = $('<div class="per">' + (item[1] * 100) + '%div');
+
+        name.append(rate);
+        point.append(name);
+
+        var per = (item[1] / base * 100) + '%';
+        point.width(per).height(per);
+
+        if (item[2]) {
+            point.css('background-color', item[2])
+        }
+        if (item[3] !== undefined && item[4] !== undefined) {
+            point.css('left', item[3]).css('top', item[4]);
+        }
+
+        component.append(point);
+    });
+
+    return component;
+}
